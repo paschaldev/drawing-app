cf31cffd2a39c65c3950afe7079ed0c36782bc85
Paschal Ezeugwu <ezeugwupaschal@gmail.com>
2023-03-17 10:25:11 +0100

    Load toolbars from an array

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,7 @@
 import LyraStore from 'src/store';
 import AppContext from 'src/contexts';
 import Toolbar from 'src/components/Toolbar';
+import tools from 'src/data';
 
 // Mob-X store instance
 const store = new LyraStore();
@@ -9,7 +10,7 @@ const App = () => {
   return (
     <div className="app">
       <AppContext.Provider value={store}>
-        <Toolbar />
+        <Toolbar tools={tools} />
       </AppContext.Provider>
     </div>
   );
